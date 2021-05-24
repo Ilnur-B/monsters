aa9329e4dbdf8be0c231d103a1fefff2fadaa252
Ilnur <ilnur.bkhtrv@gmail.com>
2021-05-24 14:52:47 +0300

    add timer

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import "./App.css";
 import React, {Component} from "react";
 
 import CardList from './component/card-list/card-list.component';
-import SearchBox from './component/serch-box/search-box.component'
+import SearchBox from './component/serch-box/search-box.component';
+import Clock from "./component/clock/clock.component";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             monsters: [],
-            searchField: ''
+            searchField: '',
+            date: new Date()
         }
     }
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({monsters:users}))
+            .then(users => this.setState({monsters:users}));
+        this.timerId = setInterval(() => this.tick(), 1000)
     }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+
+    tick() {
+    this.setState({
+        date: new Date()
+    })
+}
 
 
 
@@ -30,6 +43,9 @@ export default class App extends Component {
 
             <div className="App">
                 <h1>Monsters catalog</h1>
+                <Clock
+                    date={this.state.date.toLocaleTimeString()}
+                />
                 <SearchBox
                     placeholder='search monster'
                     handler={e => this.setState({searchField: e.target.value})}
---
+++ src/component/clock/clock.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Clock = (props) => {
+    return(
+        <div>
+            <h3> сейчас {props.date}</h3>
+        </div>
+    )
+
+}
+export default Clock
